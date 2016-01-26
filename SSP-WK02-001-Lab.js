7aca94de384f1317f732112b8adb8cf2f5db7bd7
daydreamer102 <eiren.mcloughlin@gmail.com>
2016-01-26 15:16:42 +0000

    fixed it but i think there many still be an error

--- SSP-WK02-001-Lab.js
+++ SSP-WK02-001-Lab.js
@@ -8,12 +8,16 @@ fs.readdir("./", function(err, files) {
 	
 		if(files[i] != "SSP-WK02-001-Lab.js"){    
 			fs.readFile(files[i], {encoding: "utf8", flag: "r"}, function(error, data){
-				if(error) { 
+				/*if(error) { 
 					throw error;        
-				}
+				}*/ 
+				//For some reason the if statement is causing it to stop because something must be calling 
+				//an error but when commented out it is fine and consoles out the file contents, for the 
+				//moment I am commenting it out
 				
-				console.log("Contents of " + files[i] + "\n\n" + data); 
+				console.log("Contents of " + files[i] + data); 
 				c++;
+
 			});
 		}
 	}
