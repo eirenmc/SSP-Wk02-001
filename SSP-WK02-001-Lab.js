7f5d1e0ea28e7f0caa3ba09894815ae8e4b6309e
daydreamer102 <eiren.mcloughlin@gmail.com>
2016-01-26 15:05:21 +0000

    got the names to print out but not the content, still working on it

--- SSP-WK02-001-Lab.js
+++ SSP-WK02-001-Lab.js
@@ -1,11 +1,22 @@
 var fs = require("fs");
+var c = 0;
 
-fs.readdir("./Files/", function(err, files) {
+fs.readdir("./", function(err, files) {
 	
-	var readFileContents = function(aFileName) {
-      fs.readFile(aFileName,{encoding:"utf8"}, function(error, data) {
-		  
-        fileContent.push({name: aFileName, contents: data});
-		for(var i in files){
-			if(files[i] != "SSP-WK02-001-Lab.js")
+	for(var i in files){
+		console.log("File:" + files[i]);
+	
+		if(files[i] != "SSP-WK02-001-Lab.js"){    
+			fs.readFile(files[i], {encoding: "utf8", flag: "r"}, function(error, data){
+				if(error) { 
+					throw error;        
+				}
+				
+				console.log("Contents of " + files[i] + "\n\n" + data); 
+				c++;
+			});
 		}
+	}
+});	
+
+	
